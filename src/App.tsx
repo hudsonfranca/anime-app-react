26f2253dc5943c8880defbabd138428b7437c8e9
Hudson França <hudsonsilvares@gmail.com>
2020-07-03 18:52:32 -0300

    Layout component created.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Layout } from './components/Layout';
+import { ResetStyle, GlobalStyle } from './components/GlobalStyle/globalStyle';
 
-function App() {
-  return <div className="App"></div>;
-}
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <ResetStyle />
+      <GlobalStyle />
+      <Layout />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/Layout/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Grid = styled.div`
+  width: 100%;
+  height: 100%;
+  grid-template-areas:
+    'navbar navbar navbar'
+    'main main sidebar'
+    'footer footer footer';
+`;
